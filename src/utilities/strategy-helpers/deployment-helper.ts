1b6941228ac54166f68312e823424fe0feda335b
Deepak Sattiraju <desattir@microsoft.com>
2019-11-14 15:02:00 +0530

    Deployment Strategy (#4)

---
+++ src/utilities/strategy-helpers/deployment-helper.ts
@@ -0,0 +1,161 @@
+'use strict';
+
+import * as fs from 'fs';
+import * as path from 'path';
+import * as core from '@actions/core';
+import * as yaml from 'js-yaml';
+import * as canaryDeploymentHelper from './canary-deployment-helper';
+import * as KubernetesObjectUtility from '../resource-object-utility';
+import * as TaskInputParameters from '../../input-parameters';
+import * as models from '../../constants';
+import * as fileHelper from '../files-helper';
+import * as utils from '../manifest-utilities';
+import * as KubernetesManifestUtility from '../manifest-stability-utility';
+import * as KubernetesConstants from '../../constants';
+import { Kubectl, Resource } from '../../kubectl-object-model';
+
+import { deployPodCanary } from './pod-canary-deployment-helper';
+import { deploySMICanary } from './smi-canary-deployment-helper';
+import { checkForErrors } from "../utility";
+
+
+export async function deploy(kubectl: Kubectl, manifestFilePaths: string[], deploymentStrategy: string) {
+
+    // get manifest files
+    let inputManifestFiles: string[] = getManifestFiles(manifestFilePaths);
+
+    // artifact substitution
+    inputManifestFiles = updateContainerImagesInManifestFiles(inputManifestFiles, TaskInputParameters.containers);
+
+    // imagePullSecrets addition
+    inputManifestFiles = updateImagePullSecretsInManifestFiles(inputManifestFiles, TaskInputParameters.imagePullSecrets);
+
+    // deployment
+    const deployedManifestFiles = deployManifests(inputManifestFiles, kubectl, isCanaryDeploymentStrategy(deploymentStrategy));
+
+    // check manifest stability
+    const resourceTypes: Resource[] = KubernetesObjectUtility.getResources(deployedManifestFiles, models.deploymentTypes.concat([KubernetesConstants.DiscoveryAndLoadBalancerResource.service]));
+    await checkManifestStability(kubectl, resourceTypes);
+
+    // print ingress resources
+    const ingressResources: Resource[] = KubernetesObjectUtility.getResources(deployedManifestFiles, [KubernetesConstants.DiscoveryAndLoadBalancerResource.ingress]);
+    ingressResources.forEach(ingressResource => {
+        kubectl.getResource(KubernetesConstants.DiscoveryAndLoadBalancerResource.ingress, ingressResource.name);
+    });
+}
+
+function getManifestFiles(manifestFilePaths: string[]): string[] {
+    const files: string[] = utils.getManifestFiles(manifestFilePaths);
+
+    if (files == null || files.length === 0) {
+        throw new Error(`ManifestFileNotFound : ${manifestFilePaths}`);
+    }
+
+    return files;
+}
+
+function deployManifests(files: string[], kubectl: Kubectl, isCanaryDeploymentStrategy: boolean): string[] {
+    let result;
+    if (isCanaryDeploymentStrategy) {
+        let canaryDeploymentOutput: any;
+        if (canaryDeploymentHelper.isSMICanaryStrategy()) {
+            canaryDeploymentOutput = deploySMICanary(kubectl, files);
+        } else {
+            canaryDeploymentOutput = deployPodCanary(kubectl, files);
+        }
+        result = canaryDeploymentOutput.result;
+        files = canaryDeploymentOutput.newFilePaths;
+    } else {
+        if (canaryDeploymentHelper.isSMICanaryStrategy()) {
+            const updatedManifests = appendStableVersionLabelToResource(files, kubectl);
+            result = kubectl.apply(updatedManifests);
+        }
+        else {
+            result = kubectl.apply(files);
+        }
+    }
+    checkForErrors([result]);
+    return files;
+}
+
+function appendStableVersionLabelToResource(files: string[], kubectl: Kubectl): string[] {
+    const manifestFiles = [];
+    const newObjectsList = [];
+
+    files.forEach((filePath: string) => {
+        const fileContents = fs.readFileSync(filePath);
+        yaml.safeLoadAll(fileContents, function (inputObject) {
+            const kind = inputObject.kind;
+            if (KubernetesObjectUtility.isDeploymentEntity(kind)) {
+                const updatedObject = canaryDeploymentHelper.markResourceAsStable(inputObject);
+                newObjectsList.push(updatedObject);
+            } else {
+                manifestFiles.push(filePath);
+            }
+        });
+    });
+
+    const updatedManifestFiles = fileHelper.writeObjectsToFile(newObjectsList);
+    manifestFiles.push(...updatedManifestFiles);
+    return manifestFiles;
+}
+
+async function checkManifestStability(kubectl: Kubectl, resources: Resource[]): Promise<void> {
+    await KubernetesManifestUtility.checkManifestStability(kubectl, resources);
+}
+
+function updateContainerImagesInManifestFiles(filePaths: string[], containers: string[]): string[] {
+    if (!!containers && containers.length > 0) {
+        const newFilePaths = [];
+        const tempDirectory = fileHelper.getTempDirectory();
+        filePaths.forEach((filePath: string) => {
+            let contents = fs.readFileSync(filePath).toString();
+            containers.forEach((container: string) => {
+                let imageName = container.split(':')[0];
+                if (imageName.indexOf('@') > 0) {
+                    imageName = imageName.split('@')[0];
+                }
+                if (contents.indexOf(imageName) > 0) {
+                    contents = utils.substituteImageNameInSpecFile(contents, imageName, container);
+                }
+            });
+
+            const fileName = path.join(tempDirectory, path.basename(filePath));
+            fs.writeFileSync(
+                path.join(fileName),
+                contents
+            );
+            newFilePaths.push(fileName);
+        });
+
+        return newFilePaths;
+    }
+
+    return filePaths;
+}
+
+function updateImagePullSecretsInManifestFiles(filePaths: string[], imagePullSecrets: string[]): string[] {
+    if (!!imagePullSecrets && imagePullSecrets.length > 0) {
+        const newObjectsList = [];
+        filePaths.forEach((filePath: string) => {
+            const fileContents = fs.readFileSync(filePath).toString();
+            yaml.safeLoadAll(fileContents, function (inputObject: any) {
+                if (!!inputObject && !!inputObject.kind) {
+                    const kind = inputObject.kind;
+                    if (KubernetesObjectUtility.isWorkloadEntity(kind)) {
+                        KubernetesObjectUtility.updateImagePullSecrets(inputObject, imagePullSecrets, false);
+                    }
+                    newObjectsList.push(inputObject);
+                }
+            });
+        });
+        core.debug('New K8s objects after addin imagePullSecrets are :' + JSON.stringify(newObjectsList));
+        const newFilePaths = fileHelper.writeObjectsToFile(newObjectsList);
+        return newFilePaths;
+    }
+    return filePaths;
+}
+
+function isCanaryDeploymentStrategy(deploymentStrategy: string): boolean {
+    return deploymentStrategy != null && deploymentStrategy.toUpperCase() === canaryDeploymentHelper.CANARY_DEPLOYMENT_STRATEGY.toUpperCase();
+}
---
+++ src/utilities/strategy-helpers/smi-canary-deployment-helper.ts
@@ -0,0 +1,215 @@
+'use strict';
+
+import { Kubectl } from '../../kubectl-object-model';
+import * as core from '@actions/core';
+import * as fs from 'fs';
+import * as yaml from 'js-yaml';
+import * as util from 'util';
+
+import * as TaskInputParameters from '../../input-parameters';
+import * as fileHelper from '../files-helper';
+import * as helper from '../resource-object-utility';
+import * as utils from '../manifest-utilities';
+import * as canaryDeploymentHelper from './canary-deployment-helper';
+import { checkForErrors } from "../utility";
+
+const TRAFFIC_SPLIT_OBJECT_NAME_SUFFIX = '-workflow-rollout';
+const TRAFFIC_SPLIT_OBJECT = 'TrafficSplit';
+
+export function deploySMICanary(kubectl: Kubectl, filePaths: string[]) {
+    const newObjectsList = [];
+    const canaryReplicaCount = parseInt(TaskInputParameters.baselineAndCanaryReplicas);
+    core.debug('Replica count is ' + canaryReplicaCount);
+
+    filePaths.forEach((filePath: string) => {
+        const fileContents = fs.readFileSync(filePath);
+        yaml.safeLoadAll(fileContents, function (inputObject) {
+            const name = inputObject.metadata.name;
+            const kind = inputObject.kind;
+            if (helper.isDeploymentEntity(kind)) {
+                // Get stable object
+                core.debug('Querying stable object');
+                const stableObject = canaryDeploymentHelper.fetchResource(kubectl, kind, name);
+                if (!stableObject) {
+                    core.debug('Stable object not found. Creating only canary object');
+                    // If stable object not found, create canary deployment.
+                    const newCanaryObject = canaryDeploymentHelper.getNewCanaryResource(inputObject, canaryReplicaCount);
+                    core.debug('New canary object is: ' + JSON.stringify(newCanaryObject));
+                    newObjectsList.push(newCanaryObject);
+                } else {
+                    if (!canaryDeploymentHelper.isResourceMarkedAsStable(stableObject)) {
+                        throw (`StableSpecSelectorNotExist : ${name}`);
+                    }
+
+                    core.debug('Stable object found. Creating canary and baseline objects');
+                    // If canary object not found, create canary and baseline object.
+                    const newCanaryObject = canaryDeploymentHelper.getNewCanaryResource(inputObject, canaryReplicaCount);
+                    const newBaselineObject = canaryDeploymentHelper.getNewBaselineResource(stableObject, canaryReplicaCount);
+                    core.debug('New canary object is: ' + JSON.stringify(newCanaryObject));
+                    core.debug('New baseline object is: ' + JSON.stringify(newBaselineObject));
+                    newObjectsList.push(newCanaryObject);
+                    newObjectsList.push(newBaselineObject);
+                }
+            } else {
+                // Updating non deployment entity as it is.
+                newObjectsList.push(inputObject);
+            }
+        });
+    });
+
+    const manifestFiles = fileHelper.writeObjectsToFile(newObjectsList);
+    const result = kubectl.apply(manifestFiles);
+    createCanaryService(kubectl, filePaths);
+    return { 'result': result, 'newFilePaths': manifestFiles };
+}
+
+function createCanaryService(kubectl: Kubectl, filePaths: string[]) {
+    const newObjectsList = [];
+    const trafficObjectsList = [];
+
+    filePaths.forEach((filePath: string) => {
+        const fileContents = fs.readFileSync(filePath);
+        yaml.safeLoadAll(fileContents, function (inputObject) {
+
+            const name = inputObject.metadata.name;
+            const kind = inputObject.kind;
+            if (helper.isServiceEntity(kind)) {
+                const newCanaryServiceObject = canaryDeploymentHelper.getNewCanaryResource(inputObject);
+                core.debug('New canary service object is: ' + JSON.stringify(newCanaryServiceObject));
+                newObjectsList.push(newCanaryServiceObject);
+
+                const newBaselineServiceObject = canaryDeploymentHelper.getNewBaselineResource(inputObject);
+                core.debug('New baseline object is: ' + JSON.stringify(newBaselineServiceObject));
+                newObjectsList.push(newBaselineServiceObject);
+
+                core.debug('Querying for stable service object');
+                const stableObject = canaryDeploymentHelper.fetchResource(kubectl, kind, canaryDeploymentHelper.getStableResourceName(name));
+                if (!stableObject) {
+                    const newStableServiceObject = canaryDeploymentHelper.getStableResource(inputObject);
+                    core.debug('New stable service object is: ' + JSON.stringify(newStableServiceObject));
+                    newObjectsList.push(newStableServiceObject);
+
+                    core.debug('Creating the traffic object for service: ' + name);
+                    const trafficObject = createTrafficSplitManifestFile(name, 0, 0, 1000);
+                    core.debug('Creating the traffic object for service: ' + trafficObject);
+                    trafficObjectsList.push(trafficObject);
+                } else {
+                    let updateTrafficObject = true;
+                    const trafficObject = canaryDeploymentHelper.fetchResource(kubectl, TRAFFIC_SPLIT_OBJECT, getTrafficSplitResourceName(name));
+                    if (trafficObject) {
+                        const trafficJObject = JSON.parse(JSON.stringify(trafficObject));
+                        if (trafficJObject && trafficJObject.spec && trafficJObject.spec.backends) {
+                            trafficJObject.spec.backends.forEach((s) => {
+                                if (s.service === canaryDeploymentHelper.getCanaryResourceName(name) && s.weight === "1000m") {
+                                    core.debug('Update traffic objcet not required');
+                                    updateTrafficObject = false;
+                                }
+                            })
+                        }
+                    }
+
+                    if (updateTrafficObject) {
+                        core.debug('Stable service object present so updating the traffic object for service: ' + name);
+                        trafficObjectsList.push(updateTrafficSplitObject(name));
+                    }
+                }
+            }
+        });
+    });
+
+    const manifestFiles = fileHelper.writeObjectsToFile(newObjectsList);
+    manifestFiles.push(...trafficObjectsList);
+    const result = kubectl.apply(manifestFiles);
+    checkForErrors([result]);
+}
+
+export function redirectTrafficToCanaryDeployment(kubectl: Kubectl, manifestFilePaths: string[]) {
+    adjustTraffic(kubectl, manifestFilePaths, 0, 1000);
+}
+
+export function redirectTrafficToStableDeployment(kubectl: Kubectl, manifestFilePaths: string[]) {
+    adjustTraffic(kubectl, manifestFilePaths, 1000, 0);
+}
+
+function adjustTraffic(kubectl: Kubectl, manifestFilePaths: string[], stableWeight: number, canaryWeight: number) {
+    // get manifest files
+    const inputManifestFiles: string[] = utils.getManifestFiles(manifestFilePaths);
+
+    if (inputManifestFiles == null || inputManifestFiles.length == 0) {
+        return;
+    }
+
+    const trafficSplitManifests = [];
+    const serviceObjects = [];
+    inputManifestFiles.forEach((filePath: string) => {
+        const fileContents = fs.readFileSync(filePath);
+        yaml.safeLoadAll(fileContents, function (inputObject) {
+            const name = inputObject.metadata.name;
+            const kind = inputObject.kind;
+            if (helper.isServiceEntity(kind)) {
+                trafficSplitManifests.push(createTrafficSplitManifestFile(name, stableWeight, 0, canaryWeight));
+                serviceObjects.push(name);
+            }
+        });
+    });
+
+    if (trafficSplitManifests.length <= 0) {
+        return;
+    }
+
+    const result = kubectl.apply(trafficSplitManifests);
+    core.debug('serviceObjects:' + serviceObjects.join(',') + ' result:' + result);
+    checkForErrors([result]);
+}
+
+function updateTrafficSplitObject(serviceName: string): string {
+    const percentage = parseInt(TaskInputParameters.canaryPercentage) * 10;
+    const baselineAndCanaryWeight = percentage / 2;
+    const stableDeploymentWeight = 1000 - percentage;
+    core.debug('Creating the traffic object with canary weight: ' + baselineAndCanaryWeight + ',baseling weight: ' + baselineAndCanaryWeight + ',stable: ' + stableDeploymentWeight);
+    return createTrafficSplitManifestFile(serviceName, stableDeploymentWeight, baselineAndCanaryWeight, baselineAndCanaryWeight);
+}
+
+function createTrafficSplitManifestFile(serviceName: string, stableWeight: number, baselineWeight: number, canaryWeight: number): string {
+    const smiObjectString = getTrafficSplitObject(serviceName, stableWeight, baselineWeight, canaryWeight);
+    const manifestFile = fileHelper.writeManifestToFile(smiObjectString, TRAFFIC_SPLIT_OBJECT, serviceName);
+    if (!manifestFile) {
+        throw new Error('UnableToCreateTrafficSplitManifestFile');
+    }
+
+    return manifestFile;
+}
+
+function getTrafficSplitObject(name: string, stableWeight: number, baselineWeight: number, canaryWeight: number): string {
+    const trafficSplitObjectJson = `{
+        "apiVersion": "split.smi-spec.io/v1alpha1",
+        "kind": "TrafficSplit",
+        "metadata": {
+            "name": "%s"
+        },
+        "spec": {
+            "backends": [
+                {
+                    "service": "%s",
+                    "weight": "%sm"
+                },
+                {
+                    "service": "%s",
+                    "weight": "%sm"
+                },
+                {
+                    "service": "%s",
+                    "weight": "%sm"
+                }
+            ],
+            "service": "%s"
+        }
+    }`;
+
+    const trafficSplitObject = util.format(trafficSplitObjectJson, getTrafficSplitResourceName(name), canaryDeploymentHelper.getStableResourceName(name), stableWeight, canaryDeploymentHelper.getBaselineResourceName(name), baselineWeight, canaryDeploymentHelper.getCanaryResourceName(name), canaryWeight, name);
+    return trafficSplitObject;
+}
+
+function getTrafficSplitResourceName(name: string) {
+    return name + TRAFFIC_SPLIT_OBJECT_NAME_SUFFIX;
+}
