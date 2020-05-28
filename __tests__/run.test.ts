f5b2e62279dd2ac1a6bc4236aeced25094365c25
raghaves <raghaves@microsoft.com>
2020-05-28 11:40:15 +0530

    Fixing an issue with test runs on remote

--- __tests__/run.test.ts
+++ __tests__/run.test.ts
@@ -173,7 +173,8 @@ test("run() - deploy", async () => {
 
     //Invoke and assert
     await expect(action.run()).resolves.not.toThrow();
-    expect(deploySpy).toBeCalledWith({ "ignoreSSLErrors": false, "kubectlPath": path.join('cachepath','kubectl')+'.exe', "namespace": "v1.18.0" }, ['manifests/deployment.yaml'], undefined);
+    const expectedPath = path.join('cachepath', 'kubectl') + '.exe';
+    expect(deploySpy).toBeCalledWith({ "ignoreSSLErrors": false, "kubectlPath": expectedPath, "namespace": "v1.18.0" }, ['manifests/deployment.yaml'], undefined);
     deploySpy.mockRestore();
 });
 
