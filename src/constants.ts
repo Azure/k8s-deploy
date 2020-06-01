9b32df5c9a0699c030bede35baae5d84127cad09
Koushik Dey <kodey@microsoft.com>
2020-06-01 22:37:38 +0530

    Added changes to introduce annotations during deploy

--- src/constants.ts
+++ src/constants.ts
@@ -24,3 +24,13 @@ export class ServiceTypes {
 export const deploymentTypes: string[] = ['deployment', 'replicaset', 'daemonset', 'pod', 'statefulset'];
 export const workloadTypes: string[] = ['deployment', 'replicaset', 'daemonset', 'pod', 'statefulset', 'job', 'cronjob'];
 export const workloadTypesWithRolloutStatus: string[] = ['deployment', 'daemonset', 'statefulset'];
+
+export let workflowAnnotations = [
+    `run=${process.env['GITHUB_RUN_ID']}`,
+    `workflow="${process.env['GITHUB_WORKFLOW']}"`,
+    `jobName="${process.env['GITHUB_JOB']}"`,
+    `createdBy=${process.env['GITHUB_ACTOR']}`,
+    `runUri=https://github.com/${process.env['GITHUB_REPOSITORY']}/actions/runs/${process.env['GITHUB_RUN_ID']}`,
+    `commit=${process.env['GITHUB_SHA']}`,
+    `branch=${process.env['GITHUB_REF']}`
+];
