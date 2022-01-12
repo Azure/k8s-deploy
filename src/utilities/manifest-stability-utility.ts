a58b8bb3da5905798b438d22ef02505863db2359
Oliver King <kingoliver@microsoft.com>
2022-01-12 17:15:18 -0500

    refactor manifest stabilitiy utility

--- src/utilities/manifest-stability-utility.ts
+++ src/utilities/manifest-stability-utility.ts
@@ -1,5 +1,3 @@
-"use strict";
-
 import * as core from "@actions/core";
 import * as utils from "./utility";
 import * as KubernetesConstants from "../constants";
@@ -10,78 +8,62 @@ export async function checkManifestStability(
   resources: Resource[]
 ): Promise<void> {
   let rolloutStatusHasErrors = false;
-  const numberOfResources = resources.length;
-  for (let i = 0; i < numberOfResources; i++) {
+  for (let i = 0; i < resources.length; i++) {
     const resource = resources[i];
+
     if (
       KubernetesConstants.WORKLOAD_TYPES_WITH_ROLLOUT_STATUS.indexOf(
         resource.type.toLowerCase()
       ) >= 0
     ) {
       try {
-        var result = kubectl.checkRolloutStatus(resource.type, resource.name);
+        const result = await kubectl.checkRolloutStatus(
+          resource.type,
+          resource.name
+        );
         utils.checkForErrors([result]);
       } catch (ex) {
         core.error(ex);
-        kubectl.describe(resource.type, resource.name);
+        await kubectl.describe(resource.type, resource.name);
         rolloutStatusHasErrors = true;
       }
     }
-    if (
-      utils.isEqual(
-        resource.type,
-        KubernetesConstants.KubernetesWorkload.POD,
-        true
-      )
-    ) {
+
+    if (resource.type == KubernetesConstants.KubernetesWorkload.POD) {
       try {
         await checkPodStatus(kubectl, resource.name);
       } catch (ex) {
-        core.warning(`CouldNotDeterminePodStatus ${JSON.stringify(ex)}`);
-        kubectl.describe(resource.type, resource.name);
+        core.warning(`Could not determine pod status: ${JSON.stringify(ex)}`);
+        await kubectl.describe(resource.type, resource.name);
       }
     }
     if (
-      utils.isEqual(
-        resource.type,
-        KubernetesConstants.DiscoveryAndLoadBalancerResource.SERVICE,
-        true
-      )
+      resource.type ==
+      KubernetesConstants.DiscoveryAndLoadBalancerResource.SERVICE
     ) {
       try {
         const service = getService(kubectl, resource.name);
-        const spec = service.spec;
-        const status = service.status;
-        if (
-          utils.isEqual(
-            spec.type,
-            KubernetesConstants.ServiceTypes.LOAD_BALANCER,
-            true
-          )
-        ) {
+        const { spec, status } = service;
+        if (spec.type === KubernetesConstants.ServiceTypes.LOAD_BALANCER) {
           if (!isLoadBalancerIPAssigned(status)) {
             await waitForServiceExternalIPAssignment(kubectl, resource.name);
           } else {
-            console.log(
-              "ServiceExternalIP",
-              resource.name,
-              status.loadBalancer.ingress[0].ip
+            core.info(
+              `ServiceExternalIP ${resource.name} ${status.loadBalancer.ingress[0].ip}`
             );
           }
         }
       } catch (ex) {
         core.warning(
-          `CouldNotDetermineServiceStatus of: ${
-            resource.name
-          } Error: ${JSON.stringify(ex)}`
+          `Could not determine service status of: ${resource.name} Error: ${ex}`
         );
-        kubectl.describe(resource.type, resource.name);
+        await kubectl.describe(resource.type, resource.name);
       }
     }
   }
 
   if (rolloutStatusHasErrors) {
-    throw new Error("RolloutStatusTimedout");
+    throw new Error("Rollout status error");
   }
 }
 
@@ -91,21 +73,25 @@ export async function checkPodStatus(
 ): Promise<void> {
   const sleepTimeout = 10 * 1000; // 10 seconds
   const iterations = 60; // 60 * 10 seconds timeout = 10 minutes max timeout
+
   let podStatus;
   let kubectlDescribeNeeded = false;
   for (let i = 0; i < iterations; i++) {
     await utils.sleep(sleepTimeout);
+
     core.debug(`Polling for pod status: ${podName}`);
-    podStatus = getPodStatus(kubectl, podName);
+    podStatus = await getPodStatus(kubectl, podName);
+
     if (
-      podStatus.phase &&
-      podStatus.phase !== "Pending" &&
-      podStatus.phase !== "Unknown"
+      podStatus &&
+      podStatus?.phase !== "Pending" &&
+      podStatus?.phase !== "Unknown"
     ) {
       break;
     }
   }
-  podStatus = getPodStatus(kubectl, podName);
+
+  podStatus = await getPodStatus(kubectl, podName);
   switch (podStatus.phase) {
     case "Succeeded":
     case "Running":
@@ -130,39 +116,41 @@ export async function checkPodStatus(
   }
 
   if (kubectlDescribeNeeded) {
-    kubectl.describe("pod", podName);
+    await kubectl.describe("pod", podName);
   }
 }
 
-function getPodStatus(kubectl: Kubectl, podName: string) {
-  const podResult = kubectl.getResource("pod", podName);
+async function getPodStatus(kubectl: Kubectl, podName: string) {
+  const podResult = await kubectl.getResource("pod", podName);
   utils.checkForErrors([podResult]);
-  const podStatus = JSON.parse(podResult.stdout).status;
-  core.debug(`Pod Status: ${JSON.stringify(podStatus)}`);
-  return podStatus;
+
+  return JSON.parse(podResult.stdout).status;
 }
 
 function isPodReady(podStatus: any): boolean {
   let allContainersAreReady = true;
   podStatus.containerStatuses.forEach((container) => {
     if (container.ready === false) {
-      console.log(
+      core.info(
         `'${container.name}' status: ${JSON.stringify(container.state)}`
       );
       allContainersAreReady = false;
     }
   });
+
   if (!allContainersAreReady) {
-    core.warning("AllContainersNotInReadyState");
+    core.warning("All containers not in ready state");
   }
+
   return allContainersAreReady;
 }
 
-function getService(kubectl: Kubectl, serviceName) {
-  const serviceResult = kubectl.getResource(
+async function getService(kubectl: Kubectl, serviceName) {
+  const serviceResult = await kubectl.getResource(
     KubernetesConstants.DiscoveryAndLoadBalancerResource.SERVICE,
     serviceName
   );
+
   utils.checkForErrors([serviceResult]);
   return JSON.parse(serviceResult.stdout);
 }
@@ -175,29 +163,21 @@ async function waitForServiceExternalIPAssignment(
   const iterations = 18; // 18 * 10 seconds timeout = 3 minutes max timeout
 
   for (let i = 0; i < iterations; i++) {
-    console.log(`waitForServiceIpAssignment : ${serviceName}`);
+    core.info(`Wait for service ip assignment : ${serviceName}`);
     await utils.sleep(sleepTimeout);
-    let status = getService(kubectl, serviceName).status;
+
+    const status = (await getService(kubectl, serviceName)).status;
     if (isLoadBalancerIPAssigned(status)) {
-      console.log(
-        "ServiceExternalIP",
-        serviceName,
-        status.loadBalancer.ingress[0].ip
+      core.info(
+        `ServiceExternalIP ${serviceName} ${status.loadBalancer.ingress[0].ip}`
       );
       return;
     }
   }
-  core.warning(`waitForServiceIpAssignmentTimedOut ${serviceName}`);
+
+  core.warning(`Wait for service ip assignment timed out${serviceName}`);
 }
 
 function isLoadBalancerIPAssigned(status: any) {
-  if (
-    status &&
-    status.loadBalancer &&
-    status.loadBalancer.ingress &&
-    status.loadBalancer.ingress.length > 0
-  ) {
-    return true;
-  }
-  return false;
+  return status?.loadBalancer?.ingress?.length > 0;
 }
