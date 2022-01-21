c5754fbe28e810ff0e819bf75e3bfcfddf50407a
Oliver King <kingoliver@microsoft.com>
2022-01-21 13:49:34 -0600

    fix run.ts

--- src/run.ts
+++ src/run.ts
@@ -8,46 +8,45 @@ import {parseDeploymentStrategy} from "./types/deploymentStrategy";
 
 export async function run() {
     // verify kubeconfig is set
-    if (!process.env["KUBECONFIG"]) {
+    if (!process.env["KUBECONFIG"])
         core.warning(
             "KUBECONFIG env is not explicitly set. Ensure cluster context is set by using k8s-set-context action."
         );
 
-        // get inputs
-        const action: Action | undefined = parseAction(
-            core.getInput("action", {required: true})
-        );
-        const strategy = parseDeploymentStrategy(core.getInput("strategy"));
-        const manifestsInput = core.getInput("manifests", {required: true});
-        const manifestFilePaths = manifestsInput
-            .split(/[\n,;]+/) // split into each individual manifest
-            .map((manifest) => manifest.trim()) // remove surrounding whitespace
-            .filter((manifest) => manifest.length > 0); // remove any blanks
+    // get inputs
+    const action: Action | undefined = parseAction(
+        core.getInput("action", {required: true})
+    );
+    const strategy = parseDeploymentStrategy(core.getInput("strategy"));
+    const manifestsInput = core.getInput("manifests", {required: true});
+    const manifestFilePaths = manifestsInput
+        .split(/[\n,;]+/) // split into each individual manifest
+        .map((manifest) => manifest.trim()) // remove surrounding whitespace
+        .filter((manifest) => manifest.length > 0); // remove any blanks
 
-        // create kubectl
-        const kubectlPath = await getKubectlPath();
-        const namespace = core.getInput("namespace") || "default";
-        const kubectl = new Kubectl(kubectlPath, namespace, true);
+    // create kubectl
+    const kubectlPath = await getKubectlPath();
+    const namespace = core.getInput("namespace") || "default";
+    const kubectl = new Kubectl(kubectlPath, namespace, true);
 
-        // run action
-        switch (action) {
-            case Action.DEPLOY: {
-                await deploy(kubectl, manifestFilePaths, strategy);
-                break;
-            }
-            case Action.PROMOTE: {
-                await promote(kubectl, manifestFilePaths, strategy);
-                break;
-            }
-            case Action.REJECT: {
-                await reject(kubectl, manifestFilePaths, strategy);
-                break;
-            }
-            default: {
-                throw Error(
-                    'Not a valid action. The allowed actions are "deploy", "promote", and "reject".'
-                );
-            }
+    // run action
+    switch (action) {
+        case Action.DEPLOY: {
+            await deploy(kubectl, manifestFilePaths, strategy);
+            break;
+        }
+        case Action.PROMOTE: {
+            await promote(kubectl, manifestFilePaths, strategy);
+            break;
+        }
+        case Action.REJECT: {
+            await reject(kubectl, manifestFilePaths, strategy);
+            break;
+        }
+        default: {
+            throw Error(
+                'Not a valid action. The allowed actions are "deploy", "promote", and "reject".'
+            );
         }
     }
 }
