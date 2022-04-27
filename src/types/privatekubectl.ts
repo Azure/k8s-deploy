de45f69f766030af9cf626f42356c7b10c464bed
Marcus Hines <marcushines@Marcuss-Mac-mini.local>
2022-04-27 15:40:51 -0400

    yaml file name parser

--- src/types/privatekubectl.ts
+++ src/types/privatekubectl.ts
@@ -7,8 +7,36 @@ export class PrivateKubectl extends Kubectl{
 
 
   protected async execute(args: string[], silent: boolean = false) {
+    if (this.ignoreSSLErrors) {
+      args.push("--insecure-skip-tls-verify");
+    }
+    args = args.concat(["--namespace", this.namespace]);
+
+    core.debug(`Kubectl run with command: ${this.kubectlPath} ${args}`);
    
     return null //await  getExecOutput(super.kubectlPath, args, { silent });
+  }
+
+
+
+  private parseYamlFiles(strToParse: string) {
+    if(strToParse == null || strToParse.length == 0){
+      return "";
+    }
+    
+    var regex = new RegExp("([A-Za-z]*.yaml)", "g");
+    var match = regex.exec(strToParse);
+    var result = Array();
+    
+    while (match != null) {
+      match = regex.exec(strToParse);
+
+      if(match == null){
+        continue;
+      }
+      result.push(match[0].toString);
+    }
+    return result;
   }
 }
 
