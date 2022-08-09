89670e70934af75c5259b16caeae41b56ddbe302
Jaiveer Katariya <jaiveerkatariya@Jaiveers-MBP.lan>
2022-08-09 15:45:08 -0400

    adding tracking for reject
    Signed-off-by: Jaiveer Katariya <jaiveerkatariya@Jaiveers-MBP.lan>

--- src/strategyHelpers/blueGreen/blueGreenHelper.ts
+++ src/strategyHelpers/blueGreen/blueGreenHelper.ts
@@ -56,6 +56,7 @@ export async function deleteGreenObjects(
       }
       resourcesToDelete.push(resourceToDelete)
    })
+   core.debug('deleting green objects: ' + JSON.stringify(resourcesToDelete))
 
    await deleteObjects(kubectl, resourcesToDelete)
    return resourcesToDelete
