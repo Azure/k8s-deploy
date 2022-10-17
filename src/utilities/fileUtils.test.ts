e917b5a6660336766ffe1d761884a906a8533443
Jaiveer Katariya <35347859+jaiveerk@users.noreply.github.com>
2022-10-17 17:48:28 -0400

    Deploy with Manifests from URLs (#251)
    * added functionality, need to add/modify existing tests
    
    * added tests
    
    * updated readme
    
    * prettier

--- src/utilities/fileUtils.test.ts
+++ src/utilities/fileUtils.test.ts
@@ -1,11 +1,45 @@
-import {getFilesFromDirectories} from './fileUtils'
+import {
+   getFilesFromDirectoriesAndURLs,
+   getTempDirectory,
+   urlFileKind,
+   writeYamlFromURLToFile
+} from './fileUtils'
 
+import * as yaml from 'js-yaml'
+import * as fs from 'fs'
 import * as path from 'path'
+import {succeeded} from '../types/errorable'
 
+const sampleYamlUrl =
+   'https://raw.githubusercontent.com/kubernetes/website/main/content/en/examples/controllers/nginx-deployment.yaml'
 describe('File utils', () => {
-   it('detects files in nested directories and ignores non-manifest files and empty dirs', () => {
+   test('correctly parses a yaml file from a URL', async () => {
+      const tempFile = await writeYamlFromURLToFile(sampleYamlUrl, 0)
+      const fileContents = fs.readFileSync(tempFile).toString()
+      const inputObjects = yaml.safeLoadAll(fileContents)
+      expect(inputObjects).toHaveLength(1)
+
+      for (const obj of inputObjects) {
+         expect(obj.metadata.name).toBe('nginx-deployment')
+         expect(obj.kind).toBe('Deployment')
+      }
+   })
+
+   it('fails when a bad URL is given among other files', async () => {
+      const badUrl = 'https://www.github.com'
+
       const testPath = path.join('test', 'unit', 'manifests')
-      const testSearch: string[] = getFilesFromDirectories([testPath])
+      await expect(
+         getFilesFromDirectoriesAndURLs([testPath, badUrl])
+      ).rejects.toThrow()
+   })
+
+   it('detects files in nested directories and ignores non-manifest files and empty dirs', async () => {
+      const testPath = path.join('test', 'unit', 'manifests')
+      const testSearch: string[] = await getFilesFromDirectoriesAndURLs([
+         testPath,
+         sampleYamlUrl
+      ])
 
       const expectedManifests = [
          'test/unit/manifests/manifest_test_dir/another_layer/deep-ingress.yaml',
@@ -17,13 +51,18 @@ describe('File utils', () => {
       ]
 
       // is there a more efficient way to test equality w random order?
-      expect(testSearch).toHaveLength(7)
+      expect(testSearch).toHaveLength(8)
       expectedManifests.forEach((fileName) => {
-         expect(testSearch).toContain(fileName)
+         if (fileName.startsWith('test/unit')) {
+            expect(testSearch).toContain(fileName)
+         } else {
+            expect(fileName.includes(urlFileKind)).toBe(true)
+            expect(fileName.startsWith(getTempDirectory()))
+         }
       })
    })
 
-   it('crashes when an invalid file is provided', () => {
+   it('crashes when an invalid file is provided', async () => {
       const badPath = path.join('test', 'unit', 'manifests', 'nonexistent.yaml')
       const goodPath = path.join(
          'test',
@@ -32,12 +71,12 @@ describe('File utils', () => {
          'manifest_test_dir'
       )
 
-      expect(() => {
-         getFilesFromDirectories([badPath, goodPath])
-      }).toThrowError()
+      expect(
+         getFilesFromDirectoriesAndURLs([badPath, goodPath])
+      ).rejects.toThrowError()
    })
 
-   it("doesn't duplicate files when nested dir included", () => {
+   it("doesn't duplicate files when nested dir included", async () => {
       const outerPath = path.join('test', 'unit', 'manifests')
       const fileAtOuter = path.join(
          'test',
@@ -53,11 +92,16 @@ describe('File utils', () => {
       )
 
       expect(
-         getFilesFromDirectories([outerPath, fileAtOuter, innerPath])
+         await getFilesFromDirectoriesAndURLs([
+            outerPath,
+            fileAtOuter,
+            innerPath
+         ])
       ).toHaveLength(7)
    })
-})
 
-// files that don't exist / nested files that don't exist / something else with non-manifest
-// lots of combinations of pointing to a directory and non yaml/yaml file
-// similarly named files in different folders
+   it('throws an error for an invalid URL', async () => {
+      const badUrl = 'https://www.github.com'
+      await expect(writeYamlFromURLToFile(badUrl, 0)).rejects.toBeTruthy()
+   })
+})
