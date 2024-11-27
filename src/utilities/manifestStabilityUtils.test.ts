c418a7c750a0d2e711065659979f42293da4d90c
audrastump <stumpaudra@microsoft.com>
2024-11-27 10:36:06 -0800

    tidying up

--- src/utilities/manifestStabilityUtils.test.ts
+++ src/utilities/manifestStabilityUtils.test.ts
@@ -1,13 +1,8 @@
-import * as fileUtils from './fileUtils'
 import * as manifestStabilityUtils from './manifestStabilityUtils'
-import * as path from 'path'
-import * as fs from 'fs'
-import {before} from 'node:test'
 import {Kubectl} from '../types/kubectl'
 
 describe('manifestStabilityUtils', () => {
-
-   const resourceType = 'microsoft.containerservice/fleets'
+   const kc = new Kubectl('')
    const resources = [
       {
          type: 'deployment',
@@ -15,17 +10,14 @@ describe('manifestStabilityUtils', () => {
          namespace: 'default'
       }
    ]
-   const kc = new Kubectl('')
+   const resourceType = 'microsoft.containerservice/fleets'
 
    it('should return immediately if the resource type is microsoft.containerservice/fleets', async () => {
       const spy = jest.spyOn(manifestStabilityUtils, 'checkManifestStability')
       const checkRolloutStatusSpy = jest.spyOn(kc, 'checkRolloutStatus')
-
       await manifestStabilityUtils.checkManifestStability(kc, resources, resourceType)
 
       expect(checkRolloutStatusSpy).not.toHaveBeenCalled()
       expect(spy).toHaveReturned()
    })
-
-
 })
